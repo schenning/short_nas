5ed5905bdb9bb63d975d7ed55b02126479e99c72
Henning Schei <henning.schei@gmail.com>
2023-11-06 14:05:16 +0100

    Remove test section

--- nas.py
+++ nas.py
@@ -6,12 +6,12 @@ url = 'https://ssr.finanstilsynet.no/Home/Details/NO0010196140'
 
 response = requests.get(url)
 
-# Check if the request was successful (status code 200)
+
 if response.status_code == 200:
-    # Parse the HTML content of the page using BeautifulSoup
+    
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Example: Find and print the text inside a specific HTML element
+    
    
     element = soup.find_all('div', {'class': 'container'})
     if element:
@@ -26,30 +26,3 @@ if response.status_code == 200:
 else:
     print("Failed to retrieve the web page. Status code:", response.status_code)
 
-
-position_data = []
-current_position = {}
-data_list = x
-for item in data_list:
-    key = 'Position Holder'
-    if key not in current_position:
-        current_position[key] = item
-    else:
-        current_position[key] = item
-        key = 'Short Position'
-    if key not in current_position:
-        current_position[key] = item
-    else:
-        current_position[key] = item
-        key = 'Short Percent'
-    if key not in current_position:
-        current_position[key] = item
-    else:
-        current_position[key] = item
-        key = 'Position Date (dd.mm.yyyy)'
-    if key not in current_position:
-        current_position[key] = item
-
-# Print the list of dictionaries
-for position in position_data:
-    print(position)
