035780147232fe265fd46312130721ccfa67e83b
Henning Schei <henning.schei@gmail.com>
2023-11-17 19:25:13 +0100

    bump

--- nas.py
+++ nas.py
@@ -1,11 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 url = 'https://ssr.finanstilsynet.no/Home/Details/NO0010196140'
 
-url = 'https://ssr.finanstilsynet.no/api/v2/instruments'
+url = 'https://ssr.finanstilsynet.no/api/v2/instruments/export-json'
 response = requests.get(url)
 
 
 if response.status_code == 200:
-    print (response.text)
+    json_data = response.text
+   
+    target_isin = "NO0010196140"
+    for elem in json_data:
+        print (elem)
