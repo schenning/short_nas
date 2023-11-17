115646d82f0109eb58b58f3ed6fa8f1002348177
Henning Schei <henning.schei@gmail.com>
2023-11-17 13:36:12 +0100

    asd

--- nas.py
+++ nas.py
@@ -3,3 +3,9 @@ from bs4 import BeautifulSoup
 
 url = 'https://ssr.finanstilsynet.no/Home/Details/NO0010196140'
 
+url = 'https://ssr.finanstilsynet.no/api/v2/instruments'
+response = requests.get(url)
+
+
+if response.status_code == 200:
+    print (response.text)
